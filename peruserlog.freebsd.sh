23dd77257405a7c7ddef3d9147d68502d08dce86
Phil Kulin <schors@gmail.com>
2013-03-31 16:24:30 +0400

    final add

--- peruserlog.freebsd.sh
+++ peruserlog.freebsd.sh
@@ -4,6 +4,20 @@
 # REQUIRE: NETWORKING
 # BEFORE: LOGIN
 
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
 . /etc/rc.subr
 
 name=peruserlog
@@ -11,10 +25,10 @@ rcvar=`set_rcvar`
 
 pidfile=/var/run/peruserlog.pid
 command_interpreter="/usr/local/bin/perl"
-procname="/root/server/peruserlog"
+procname="/usr/local/bin/peruserlog"
 
 command="/usr/sbin/daemon"
-command_args=" -f -p ${pidfile} /root/server/peruserlog"
+command_args=" -f -p ${pidfile} ${procname}"
 
 load_rc_config $name
 
